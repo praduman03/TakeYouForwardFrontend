916ea5a5a952097a4471aec6a6abfeb7a0fff21c
praduman <praduman03k@gmail.com>
2024-03-20 20:52:18 +0530

    minor change

--- src/app/submissions/page.tsx
+++ src/app/submissions/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { useEffect, useState } from "react";
-import "./Submissions.css";
+import "./submissions.css";
 import Navbar from "../component/Navbar";
 
 const Submissions = () => {
